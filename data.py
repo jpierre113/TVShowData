055d6c632ba11cf71678cde4eef7f349b85ef80b
Janna Pierre <gojanna@aol.com>
2021-04-11 13:18:28 -0400

    Able to return boolean based on hometown

--- data.py
+++ data.py
@@ -1,5 +1,14 @@
 import matplotlib.pyplot as pyplot
 import pandas as pd
 
-print(pd.__version__)
-# df_viewers = pd.read_csv('Software Engineering Programming Exercise.csv')
+#reads the CSV file using Pandas
+df_viewers = pd.read_csv('Software Engineering Programming Exercise.csv')
+
+#sets variable to a dataframe
+viewer_frame = pd.DataFrame(df_viewers)
+
+isPB = viewer_frame['Viewer Hometown'] == 'Pittsburgh'
+isCL = viewer_frame['Viewer Hometown'] == 'Cleveland'
+
+
+# plt.bar(x = df_viewers['Program Genre'])
